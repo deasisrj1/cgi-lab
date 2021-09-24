8faa9831e51d0f11272d6ca0519c502994dc4485
deasisrj1 <rdeasis@ualberta.ca>
2021-09-24 16:04:21 -0600

    add cookie

--- login.py
+++ login.py
@@ -5,18 +5,22 @@ import cgitb
 import cgi
 import templates
 import secret
+import os
+
 cgitb.enable()
 form = cgi.FieldStorage()
 
 username = form.getvalue('username')
 password = form.getvalue('password')
 
-print("Content-Type: text/html")
-print()
+
 if username == secret.username and password == secret.password:
     print("Set-Cookie:username = %s;" % username)
     print("Set-Cookie:password = %s;" % password)
     print("Set-Cookie:Expires = Tuesday, 31-Dec-2022 23:12:40 GMT;")
+    print("Set-Cookie:Domain = http://localhost:8080/")
+    print("Content-type:text/html\r\n")
+    print()
     print(templates.secret_page(username=username, password=password))
 else:
     print(templates.after_login_incorrect())
